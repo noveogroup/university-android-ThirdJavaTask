e476b34265e71dcab49c322b681986f832cf2858
Aleksandr Vaytsekhovskiy <Aleksandr.Vaytsekhovskiy@noveogroup.com>
2018-06-29 14:32:52 +0700

    Provide Kotlin Any.wait/notify functions

--- app/src/main/kotlin/com/noveogroup/main/Main.kt
+++ app/src/main/kotlin/com/noveogroup/main/Main.kt
@@ -1,5 +1,11 @@
+@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 package com.noveogroup.main
 
 fun main(args: Array<String>) {
     //You can check your solution here.
 }
+
+//Kotlin hides wait/notify from developer by default.
+fun Any.wait() = (this as java.lang.Object).wait()
+fun Any.notifyAll() = (this as java.lang.Object).notifyAll()
+fun Any.notify() = (this as java.lang.Object).notify()
